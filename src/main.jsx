55f10ede9aedada8c247a2549504a5b312e9aa1a
undefined <etimpaul22@gmail.com>
2025-11-29 12:59:12 +0100

    wagmi set up

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,9 @@ import IP_Monitoring from './pages/IP_Monitoring';
 import Onchain_IP_History from './pages/Onchain_IP_History';
 import Dispute_Management from './pages/Dispute_Management';
 import Header from './components/header';
+import { WagmiProvider } from 'wagmi';
+import { config } from './config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -38,10 +41,16 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className=''>
-      <RouterProvider router={router} />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </WagmiProvider>
     </div>
   </StrictMode>,
 )
